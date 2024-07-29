75405ec58f0402afd07a514f84bbcf0d1ed5521d
julievictoria98 <jura0002@stud.kea.dk>
2024-07-29 12:24:56 +0200

    onClick knapper begyndt

--- src/pages/CV.tsx
+++ src/pages/CV.tsx
@@ -3,6 +3,7 @@ import JSInfo from "../components/JSInfo";
 
 function CV() {
   const [showJSInfo, setShowJSInfo] = useState(false);
+  const [showCSSInfo, setShowCSSInfo] = useState(false);
 
   return (
     <div className="">
@@ -11,9 +12,23 @@ function CV() {
       <button onClick={() => setShowJSInfo(!showJSInfo)}>
         {showJSInfo ? "^" : "v"}
       </button>
+      <button onClick={() => setShowCSSInfo(!showCSSInfo)}>
+        {showCSSInfo ? "^" : "v"}
+      </button>
       {showJSInfo && (
         <div>
           <JSInfo />
+        </div>
+      )}
+      {showCSSInfo && (
+        <div>
+          <h1>CSSInfo</h1>
+          <p>
+            Cascading Style Sheets (CSS) is a style sheet language used for
+            describing the presentation of a document written in a markup
+            language such as HTML. CSS is a cornerstone technology of the World
+            Wide Web, alongside HTML and JavaScript.
+          </p>
         </div>
       )}
     </div>
