c322db0ccd50fda22d2de09a0b0f8de989dc2e12
julievictoria98 <jura0002@stud.kea.dk>
2024-08-22 15:08:33 +0200

    rette tekstt

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -8,21 +8,21 @@ export function AboutMe() {
         <h1 className="h2">About me</h1>
         <div className={s.about_me_flex}>
           <p>
-            My name is Julie, and I am a Danish, passionate and curious
-            multimedia design student entering my second year at KEA. The first
-            years as a student consisted of a lot of trial and error and
-            resulted in me gaining a solid foundation in front-end development,
-            including HTML, CSS, and JavaScript, as well as an understanding of
-            design theory and conventions. During this summer vacation, I have
-            been learning the React framework, which I used to create this
-            resume. In the upcoming semester, our focus will be exclusively on
-            front-end development, with an emphasis on React. I am excited to
-            deepen my understanding of this framework that I have already come
-            to enjoy. For details on my expertise with different frameworks and
-            design tools, please see the section below. I am currently seeking a
-            part-time job where I can apply my skills in front-end development
-            and contribute to real-world projects, while continuing to grow both
-            technically and creatively
+            My name is Julie, and I am passionate and curious multimedia design
+            student entering my second year at KEA - Copenhagen school of design
+            and technology. The first years as a student consisted of a lot of
+            trial and error and resulted in me gaining a solid foundation in
+            front-end development, including HTML, CSS, and JavaScript, as well
+            as an understanding of design theory and conventions. During this
+            summer vacation, I have been learning the React framework, which I
+            used to create this resume. In the upcoming semester, our focus will
+            be exclusively on front-end development, with an emphasis on React.
+            I am excited to deepen my understanding of this framework that I
+            have already come to enjoy. For details on my expertise with
+            different frameworks and design tools, please see the section below.
+            I am currently seeking a part-time job where I can apply my skills
+            in front-end development and contribute to real-world projects,
+            while continuing to grow both technically and creatively
           </p>
           <div className={s.info_flex_column}>
             <div>
