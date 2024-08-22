cc66daa966fd400acb4ef0657f8a5749ed5d5bcd
julievictoria98 <jura0002@stud.kea.dk>
2024-08-22 12:41:04 +0200

    fikset iconer i contact og noget padding i hero ift. media

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,14 +1,22 @@
 import "../style/contact.css";
 import ContactArrow from "../components/ContactArrow";
+import { FaPhoneAlt } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
 
 function Contact() {
   return (
     <div className="contact">
       <div className="contact_flex_column">
         <ContactArrow />
-        <div className="contact-flex">
-          <p className="contact-box">mail: julieradant@gmail.com</p>
-          <p className="contact-box">Phonenumber: 50523491</p>
+        <div className="contacts-flex">
+          <div className="contact-flex">
+            <MdEmail style={{ color: "#514c45" }} />
+            <p>julieradant@gmail.com</p>
+          </div>
+          <div className="contact-flex">
+            <FaPhoneAlt style={{ color: "#514c45" }} />
+            <p> (+45) 50 52 34 91</p>
+          </div>
         </div>
       </div>
     </div>
