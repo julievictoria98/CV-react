117fd1b7b40f77511718d1dc7caf1bfa48dfe3c0
julievictoria98 <jura0002@stud.kea.dk>
2024-09-29 16:01:10 +0200

    fixed  gap between components

--- src/components/work/Folders.tsx
+++ src/components/work/Folders.tsx
@@ -8,7 +8,7 @@ function Folders() {
     <h1 className='h2'>Former Exercises</h1>
     <div className={s.folders_grid}>
         <Folder folderName="Game (first semester)" workLink="https://julievictoria98.github.io/Guardian-of-the-wild/"/>
-        <Folder folderName="Exam 2024" workLink="hej"/>
+        <Folder folderName="Assignment (third semester)" workLink="https://neon-concha-50d68d.netlify.app/casestudy/"/>
         <Folder folderName="Game (third semester)" workLink="https://julievictoria98.github.io/rockpaperscissors/"/>
     </div>
     </>
